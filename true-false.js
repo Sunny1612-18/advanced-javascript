a9ae7108f32c2c5b79190275dddde30ddf53ceea
Sunny1612-18 <sunny01680144850@gmail.com>
2020-11-07 15:22:32 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,15 @@
+//const age="4";
+//if(age>0){
+    //console.log("condition is true");
+}
+//else{
+    //console.log("condition is false");
+}
+
+let name=12;
+if(name || Name == 0){
+console.log("condition is a true");
+}
+else{
+    console.log("condition is a false");
+}
