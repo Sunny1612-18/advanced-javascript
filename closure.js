39884832f56a4e277d4b4b4773b4048f0ab00d5f
Sunny1612-18 <sunny01680144850@gmail.com>
2020-11-08 01:34:56 +0600

    closure

---
+++ closure.js
@@ -0,0 +1,12 @@
+function stopwatch(){
+    let count =0;
+    return function(){
+        count++;
+        return counts
+    }
+}
+   const clock1=stopwatch();
+   console.log(clock1());
+   console.log(clock1());
+   console.log(clock1());
+   console.log(clock1());
